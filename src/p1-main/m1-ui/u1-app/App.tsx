0d29a25b126ab78945367545fa7c22a55fc4d595
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2022-08-26 22:08:14 +0300

    'HW5prt5'

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -4,7 +4,7 @@ import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
     return (
-        <Router >
+        <Router>
             <div>react homeworks:</div>
             <HW5/>
         </Router>
